3223103e1f68d1945c256fdf349e2b342d1ae540
AniruddhaNandyWGT <aniruddhanandy@woodgrovetech.com>
2025-06-16 15:17:24 -0400

    further syntax fix

--- src/IdentityServerAspNetIdentity/Services/CustomUserClaimsPrincipalFactory.cs
+++ src/IdentityServerAspNetIdentity/Services/CustomUserClaimsPrincipalFactory.cs
@@ -5,37 +5,20 @@ using System.Security.Claims;
 
 namespace IdentityServerAspNetIdentity.Services;
 
-public class CustomUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser, ApplicationRole>
+public class CustomUserClaimsPrincipalFactory(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, IOptions<IdentityOptions> optionsAccessor) : UserClaimsPrincipalFactory<ApplicationUser, ApplicationRole>(userManager, roleManager, optionsAccessor)
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly RoleManager<ApplicationRole> _roleManager;
-
-    public CustomUserClaimsPrincipalFactory(
-        UserManager<ApplicationUser> userManager,
-        RoleManager<ApplicationRole> roleManager,
-        IOptions<IdentityOptions> optionsAccessor)
-        : base(userManager, roleManager, optionsAccessor)
-    {
-        _userManager = userManager;
-        _roleManager = roleManager;
-    }
-
     protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
     {
         var identity = await base.GenerateClaimsAsync(user);
 
-        var roles = await _userManager.GetRolesAsync(user);
+        var roles = await userManager.GetRolesAsync(user);
         foreach (var roleName in roles)
         {
-            var role = await _roleManager.FindByNameAsync(roleName);
+            var role = await roleManager.FindByNameAsync(roleName);
             if (role != null)
             {
-                var roleClaims = await _roleManager.GetClaimsAsync(role);
-                foreach (var rc in roleClaims.Where(c => c.Type == "permission"))
-                {
-                    if (!identity.HasClaim(rc.Type, rc.Value))
-                        identity.AddClaim(rc);
-                }
+                var roleClaims = await roleManager.GetClaimsAsync(role);
+                foreach (var rc in roleClaims.Where(c => c.Type == "permission")) if (!identity.HasClaim(rc.Type, rc.Value)) identity.AddClaim(rc);
             }
         }
         return identity;
